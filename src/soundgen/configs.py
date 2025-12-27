da723d0acd191daeb6d4a1c91d3f677b5a95a838
Boris Podolny <bpodolnyi@gmail.com>
2025-12-28 00:30:53 +0200

    add config objects

---
+++ src/soundgen/configs.py
@@ -0,0 +1,37 @@
+import json
+from dataclasses import asdict, dataclass, field
+
+
+@dataclass
+class TrainConfig:
+    batch_size: int = 64
+    learning_rate: float = 0.001
+    num_epochs: int = 100
+
+
+@dataclass
+class VAEConfig:
+    input_shape: list[int]  # [num_channels, height, width]
+    conv_filters_number: list[int]
+    conv_kernel_size: list[int]
+    conv_strides: list[int]
+    latent_space_dim: int = 128
+    padding: int = 1
+    shape_before_bottleneck: tuple | None = None  # if None - will try to calculate based on conv layers
+    kl_weight: float = 0.0005
+
+    def __post_init__(self):
+        if isinstance(self.shape_before_bottleneck, list):
+            self.shape_before_bottleneck = tuple(self.shape_before_bottleneck)
+
+    def to_dict(self) -> dict:
+        return asdict(self)
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict(), indent=4)
+
+    @classmethod
+    def from_json(cls, path: str) -> "VAEConfig":
+        with open(path, "r") as f:
+            data = json.load(f)
+        return cls(**data)
