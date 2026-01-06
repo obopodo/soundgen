6f581df4bf6281ab4da17442bf73eca5b83b3dd7
Boris Podolny <bpodolnyi@gmail.com>
2026-01-06 14:35:09 +0200

    upd dataset

--- src/soundgen/datasets/tiny_piano.py
+++ src/soundgen/datasets/tiny_piano.py
@@ -7,14 +7,15 @@ from torch.utils.data import Dataset
 
 
 class TinyPianoDataset(Dataset):
-    def __init__(self, data_folder: str, transform: Callable | None = None):
+
+    def __init__(self, data_folder: str, transform: Callable | None = None, target_sr: int | None = 22050):
         self.data_folder = self._check_folder(data_folder)
         self.files = self._get_files(self.data_folder)
         self.transform = transform
-        self.return_raw = False
+        self.target_sr = target_sr  # if None, use original sample rate else resample
 
     def _check_folder(self, folder: str) -> Path:
-        folder = Path(folder)
+        folder: Path = Path(folder)
         if not folder.exists():
             raise FileNotFoundError(f"Folder {folder} does not exist.")
         return folder
@@ -26,8 +27,14 @@ class TinyPianoDataset(Dataset):
         return files
 
     def _load_audio(self, file_path: Path) -> tuple[Tensor, int]:
-        waveform, sample_rate = librosa.load(file_path)
+        waveform, sample_rate = librosa.load(file_path, mono=True, sr=self.target_sr)
         return waveform, sample_rate
+
+    def get_audio(self, idx: int) -> Tensor:
+        file = self.files[idx]
+        waveform, sample_rate = self._load_audio(file)
+        waveform = Tensor(waveform)
+        return waveform
 
     def _parse_label(self, file_path: Path) -> int:
         # Assuming the label is encoded in the filename as 'piano_<label>.wav'
@@ -40,10 +47,8 @@ class TinyPianoDataset(Dataset):
         waveform, sample_rate = self._load_audio(file)
         waveform = Tensor(waveform)
         label = self._parse_label(file)
-        if self.return_raw:
-            return waveform
         if self.transform:
-            waveform, s_min, s_max = self.transform(waveform, sample_rate)
+            waveform = self.transform(waveform)
         return waveform, sample_rate, label
 
     def __len__(self) -> int:
